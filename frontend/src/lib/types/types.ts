0818f1914d8c80cd95fc80dc1f64d0061b9cdd0e
mykale <masulzen@protonmail.com>
2023-10-28 10:07:59 -0400

    API mostly finished, migrated from Node to SvelteKit

--- frontend/src/lib/types/types.ts
+++ frontend/src/lib/types/types.ts
@@ -1,8 +1,23 @@
 export interface Post {
+    id: number,         // index, identifier
     slug: string;       // URL Slug
     title: string;      // Title of the article
     body: string;       // Body of the article (Markdown)
     post_date: Date;    // Original post date
     edit_date?: Date;   // Date last edited
-    author: string;     // Username of author
+    author_id: number;  // ID of author
+    tags: string[]      // tags to categorize post
+}
+
+export interface User {
+    id: number;         // index, identifier
+    username: string;   // unique username
+    password_hashed: string;
+    permissions?: Permission[] 
+}
+
+export enum Permission {
+    EDIT = "edit",
+    CREATE = "create",
+    DELETE = "delete"
 }
